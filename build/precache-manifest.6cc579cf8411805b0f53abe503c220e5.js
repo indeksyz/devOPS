534267162ae5893e9a1f7d655e928a201babad9a
codishazey@yahoo.ca <codishazey@yahoo.ca>
2020-10-14 19:04:59 -0400

    updates

---
+++ build/precache-manifest.6cc579cf8411805b0f53abe503c220e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699ccc82982d5b6789c9",
+    "url": "/indeskyz/devOPS/static/css/main.1e1bc805.chunk.css"
+  },
+  {
+    "revision": "699ccc82982d5b6789c9",
+    "url": "/indeskyz/devOPS/static/js/main.699ccc82.chunk.js"
+  },
+  {
+    "revision": "bba6d5d323eec614de39",
+    "url": "/indeskyz/devOPS/static/js/runtime~main.bba6d5d3.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/indeskyz/devOPS/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "da47da51f468c20f563e2b7b708fc6a2",
+    "url": "/indeskyz/devOPS/index.html"
+  }
+];
